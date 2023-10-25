604bf126684ab9776a13f8e2c6fa4dc87dd8e235
salineroalex <alexsalineronegro@gmail.com>
2023-10-25 20:09:09 +0200

    Optimized code for better SSH connection and synchronized new methods

--- src/dataAccess/Pool.java
+++ src/dataAccess/Pool.java
@@ -1,8 +1,5 @@
 package dataAccess;
 
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
 import exceptions.ServerErrorException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -22,11 +19,10 @@ import java.util.logging.Logger;
 public class Pool {
 
     private ResourceBundle configFile;
-    private String db_user, ssh_user;
-    private String db_pass, ssh_pass;
+    private String db_user;
+    private String db_pass;
     Stack<Connection> connections = new Stack<>();
     private static final Logger logger = Logger.getLogger(Pool.class.getName());
-    Session session = null;
 
     /**
      * Constructs a new connection pool and initializes it.
@@ -47,21 +43,16 @@ public class Pool {
      * @return A database connection.
      * @throws exceptions.ServerErrorException
      */
-    public Connection takeConnection() throws ServerErrorException {
+    public synchronized Connection takeConnection() throws ServerErrorException {
         Connection con = null;
-        
-        // For SSH sessions
-        
-        int assigned_port = openSSHConnection();
 
         //Checks if the stack is empty
-        if (connections.isEmpty() && assigned_port != 0) {
+        if (connections.isEmpty()) {
             try {
                 // DB URL with assigned SSH port
-                StringBuilder url = new StringBuilder(configFile.getString("URL"));
-                url.append(assigned_port).append(configFile.getString("DB"));
+                String url = configFile.getString("URL");
                 //Creates a new connection
-                con = DriverManager.getConnection(url.toString(), db_user, db_pass);
+                con = DriverManager.getConnection(url, db_user, db_pass);
             } catch (SQLException ex) {
                 throw new ServerErrorException();
             }
@@ -78,7 +69,7 @@ public class Pool {
      *
      * @param con The database connection to be returned to the pool.
      */
-    public void returnConnection(Connection con) {
+    public synchronized void returnConnection(Connection con) {
         //Checks if connection received is null
 
         if (con != null) {
@@ -104,45 +95,5 @@ public class Pool {
         }
         //Clears the stack
         connections.clear();
-        //Close SSH session
-        if (session != null) {
-            session.disconnect();
-        }
-    }
-
-    /**
-     * Creates an SSH connection to the postgres DB.
-     * 
-     * @return the assigned port during the port forwarding.
-     * @throws ServerErrorException if any errors have occurred.
-     */
-    private int openSSHConnection() throws ServerErrorException{
-        // Setup for the ssh user and password
-        ssh_user = configFile.getString("SSH_USER");
-        ssh_pass = configFile.getString("SSH_PASSWORD");
-        // IPs for remote host and the remote host with port forwarding.
-        String remote_host_1 = configFile.getString("REMOTE_HOST_1");
-        String remote_host_2 = configFile.getString("REMOTE_HOST_2");
-        // Database port
-        Integer postgresql_port = Integer.parseInt(configFile.getString("POSTGRESQL_PORT"));
-        // SSH port
-        Integer ssh_port = Integer.parseInt(configFile.getString("SSH_PORT"));
-        int assigned_port;
-        
-        try {
-            // Open SSH session
-            session = new JSch().getSession(ssh_user, remote_host_1, ssh_port);
-            session.setPassword(ssh_pass);
-            // Doesn't check if host is already known
-            session.setConfig("StrictHostKeyChecking", "no");
-            // Initialize connection
-            session.connect();
-            // Set port forwarding to the postgresql machine
-            assigned_port = session.setPortForwardingL(postgresql_port, remote_host_2, postgresql_port);
-        } catch (JSchException ex) {
-            throw new ServerErrorException();
-        }
-        // Returns the assigned port.
-        return assigned_port;
     }
 }
